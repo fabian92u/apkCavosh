5b8f8d8227b26e1e9db712855ec83dc3be98dc50
fabian92u <fabianuriarte2005@gmail.com>
2026-03-21 23:19:47 -0500

    Primer commit

---
+++ app/src/main/java/es/oaemdl/apkcavoshcafe202610/MainActivity.java
@@ -0,0 +1,38 @@
+package es.oaemdl.apkcavoshcafe202610;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import es.oaemdl.apkcavoshcafe202610.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        navController.addOnDestinationChangedListener((navController1, navDestination, bundle) -> {
+            int id = navDestination.getId();
+            if ( id == R.id.navigation_splash || id == R.id.navigation_login ||
+                 id == R.id.navigation_registrar )
+
+                    //id == R.id.navigation_validar ||
+                    //id == R.id.navigation_menuProducto || id == R.id.navigation_menuProductoPersonalizar )
+                navView.setVisibility( View.INVISIBLE );
+        });
+    }
+}
